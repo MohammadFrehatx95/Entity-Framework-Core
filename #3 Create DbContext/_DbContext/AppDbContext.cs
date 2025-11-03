24c53c75688f13e133c619ac9951028c6c063baf
Mohammad Frehat <mohammadfrehatx95@gmail.com>
2025-11-03 13:47:48 +0300

    Add files via upload

---
+++ #3 Create DbContext/_DbContext/AppDbContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EF_Test.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EF_Test._DbContext
+{
+    public class AppDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(Connections.sqlConStr);
+        }
+
+        public DbSet<Student> Students { get; set; }
+
+    }
+}
