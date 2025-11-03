d9406d940a44907ac399436677f403c1748bdedd
Mohammad Frehat <mohammadfrehatx95@gmail.com>
2025-11-03 13:20:40 +0300

    Add files via upload

---
+++ Connections.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EF_Test
+{
+    internal class Connections
+    {
+        public const string sqlConStr = """
+        Server=DESKTOP-I3VPEEA\SQLEXPRESS;
+        Database=EF_TestDB;
+        User Id=sa;
+        Password = 123;
+        TrustServerCertificate=True;
+        Persist Security Info=True;
+        """;
+    }
+}
